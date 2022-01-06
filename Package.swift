a1c771e33a612179239eb27c80d61575ca3b93ee
neal <keith belovay>
2022-01-06 10:56:08 -0500

    upgrade swift tools version

--- Package.swift
+++ Package.swift
@@ -1,6 +1,15 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 
 let package = Package(
-    name: "qrcode"
+    name: "qrcode",
+    products: [
+	.executable(name: "qrcode", targets: ["qrcode"])
+    ],
+    dependencies: [],
+    targets: [
+	.target(name: "qrcode", dependencies: [])
+    ]
 )
